92ab47ef9edb3b34890096b403b229940ebd3165
Donald Fox <fox_donald@yahoo.com>
2020-08-03 23:29:17 -0400

    Exercise event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
   }
 
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
-    console.log(this.includeLetters);
+    console.log('Letters: ' + this.includeLetters);
     this.password = 'MY PASSWORD!!!!';
+    console.log('Numbers: ' + this.includeNumbers);
+    console.log('Symbols: ' + this.includeSymbols);
   }
 }

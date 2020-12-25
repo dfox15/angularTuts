d98df915e83ee664d9093c7364251155f1d64684
Donald Fox <fox_donald@yahoo.com>
2020-12-24 22:01:43 -0500

    Passing data from parent to child

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() title = "";
+  @Input() imageUrl = "";
+  @Input() content = "";
+  @Input() username = "";
+  
   constructor() { }
 
   ngOnInit(): void {

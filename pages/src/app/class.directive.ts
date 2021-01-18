b324c99f0828cf5b4a3ddf380dadaac774ce5cdb
Donald Fox <fox_donald@yahoo.com>
2021-01-18 13:55:46 -0500

    Update attribute directives

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
+
 export class ClassDirective {
 
-  constructor(private element: ElementRef) { 
-    this.element.nativeElement.style.backgroundColor = 'orange'
+  constructor(private element: ElementRef) {}
+  
+  @Input('appClass') set classNames(classObj: any) {
+    for(let key in classObj) {
+      if(classObj[key]) {
+        this.element.nativeElement.classList.add(key)
+      }
+      else {
+        this.element.nativeElement.classList.remove(key)
+      }
+    }
   }
-
 }

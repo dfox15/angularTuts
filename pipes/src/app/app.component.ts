4c575210cbd0eadbb9ec2ca297c6960f64ab2c6a
Donald Fox <fox_donald@yahoo.com>
2021-05-04 21:56:48 -0400

    Pipe lecture

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -8,8 +8,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value)
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value)
+  }
 
   onNameChange(value:string) {
     this.name = value
+  }
+
+  onDateChange(value: string) {
+    this.date = value
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value)
   }
 }

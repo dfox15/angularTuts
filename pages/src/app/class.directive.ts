5dbf8dde131695a4d5a1b14f19e2cbafa0d3f0c0
Donald Fox <fox_donald@yahoo.com>
2021-05-05 13:58:01 -0400

    Using custom directives

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -6,16 +6,19 @@ import { Directive, ElementRef, Input } from '@angular/core';
 
 export class ClassDirective {
 
-  constructor(private element: ElementRef) {}
-  
-  @Input('appClass') set classNames(classObj: any) {
-    for(let key in classObj) {
-      if(classObj[key]) {
-        this.element.nativeElement.classList.add(key)
-      }
-      else {
-        this.element.nativeElement.classList.remove(key)
-      }
-    }
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.backgroundColor = 'orange'
   }
+  
+
+  // @Input('appClass') set classNames(classObj: any) {
+  //   for(let key in classObj) {
+  //     if(classObj[key]) {
+  //       this.element.nativeElement.classList.add(key)
+  //     }
+  //     else {
+  //       this.element.nativeElement.classList.remove(key)
+  //     }
+  //   }
+  // }
 }

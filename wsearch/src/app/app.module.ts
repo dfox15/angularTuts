bdfe592dbfe046a370e1156a65ea31205c54bd12
Donald Fox <fox_donald@yahoo.com>
2021-08-25 22:00:33 -0400

    Implement HTTP request

--- wsearch/src/app/app.module.ts
+++ wsearch/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -11,9 +12,7 @@ import { PageListComponent } from './page-list/page-list.component';
     SearchBarComponent,
     PageListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent]
 })

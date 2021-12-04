e9b71e388389d17a79ec1023059eb013a2fbdf54
Donald Fox <fox_donald@yahoo.com>
2021-12-04 09:59:58 -0500

    Photo app commit

---
+++ photos/src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
+
+@Component({
+  selector: 'app-photo-show',
+  templateUrl: './photo-show.component.html',
+  styleUrls: ['./photo-show.component.css']
+})
+export class PhotoShowComponent implements OnInit {
+
+  constructor(private photosService: PhotosService) { 
+    this.photosService.getPhoto().subscribe((response) => {
+      console.log(response.urls.regular)
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ photos/src/app/photos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID YZyUJCUcuE3L6G4ZHnCcbWHson-Y42U7vcsLdxihFRk'
+      }
+    })
+  }
+}

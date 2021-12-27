50f8081c0e9da4ac54ed1a44786977107dfbf499
Donald Fox <fox_donald@yahoo.com>
2021-12-27 17:14:42 -0500

    masking

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
-import { NgxMaskModule, IConfig } from 'ngx-mask'
-
-export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
@@ -17,8 +14,7 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    NgxMaskModule.forRoot()
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
